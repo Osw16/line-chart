99699f20dac359cae9712a05db5d89337e58d437
unknown <oswaldoj16@gmail.com>
2021-04-14 20:51:09 -0500

    installing dependencies and applying path

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  background:rgb(70, 69, 69);
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,57 @@
-<!-- Empty component with SVG template -->
-<template>
-  <svg>
-    <circle r="50"
-            fill="#ff6347" />
+<template> 
+  <div class="container">
+  <svg class="svg">
+    <path fill="none"
+      stroke="yellow"
+      stroke-width="5"
+      :d="d"/>
+    <circle
+      v-for="(item, index) in dataset"
+      :key="index"
+      :cx="item[0]"
+      :cy="item[1]"
+      r="50"
+      fill="#ff6347"
+      @click="onClick(item)"
+    />
   </svg>
+  </div>
 </template>
 
 <script>
-export default {}
+import * as d3 from 'd3'
+import dataset from './dataset.json'
+export default {
+  name: 'CreatingDots',
+  data() {
+    return {
+      dataset
+    }
+  },
+  methods: {
+    onClick(item) {
+      console.log('hey there', item)
+    }
+  },
+  computed:{
+    lineGenerator(){
+      return d3.line()
+      .x(v=> v[0])
+      .y(v=> v[1])
+    },
+    d(){
+      return this.lineGenerator(this.dataset)
+    }
+  }
+}
 </script>
+<style scoped>
+.container{
+  border:1px solid blueviolet;
+}
+.svg{
+  border:1px solid palevioletred;
+  height: 100vh;
+  width: 100vh;
+}
+</style>
