bf378dcd36e56e697af3240308d78412b35fc7fe
unknown <oswaldoj16@gmail.com>
2021-04-18 10:27:30 -0500

    payload added

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" >
 #app {
 
   background:rgb(202, 150, 209);
--- src/components/RealData.vue
+++ src/components/RealData.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
-      <h1>{{info}}</h1>
-      <h1>{{info_}}</h1>
+      <!-- <h1>{{info}}</h1>-->
+      <h1>{{info_}}</h1> 
       <div>
         <!-- <li v-for="item in posts" :key="item"></li> -->
-        <li v-for="item in posts" :key="item">{{item}}</li>
-
+        <!-- <li v-for="item in posts" :key="item">{{item}}</li> -->
+        <!-- <button @click="submit()">Submit</button> -->
+        <form @submit="submitir">
+          <strong>crime:</strong>
+            <input type="number" class="form-control" v-model="crime">
+          <strong>estado:</strong>
+            <input type="number" class="form-control" v-model="ent">
+          <strong>municipio:</strong>
+            <input type="number" class="form-control" v-model="mun">
+            
+          <button class="">Submit</button>
+        </form>
+          <strong>Output:</strong>
+                        <pre>
+                        {{output}}
+                        </pre>
       </div>
   </div>
   
@@ -21,20 +35,50 @@ export default {
       info: null,
       info_: null,
       items:[],
-      posts:[]
+      posts:[],
+      crime: '',
+      ent: '',
+      mun: '',
+      output:''
+
     }
   },
-    created() {
-      const data = {"id_crime":1,"id_ent":14,"id_mun1":39,};
-      const url = 'https://spotlight-unfpa.datacivica.org/api/v1/timeline';
-      axios.post(url,data)
-      .then(response => {this.posts = response.data[2];
-      this.info = response.data[2].total_ent;
-      this.info_ = response.data[5].year;
-      console.log(response.data[2])
+     methods: {
+       submit(){
+         const payload = {id_crime:1,id_ent:14,id_mun1:39};
+        const url = 'https://spotlight-unfpa.datacivica.org/api/v1/timeline';
+        axios.post(url,payload)
+        .then(response => {this.posts = response.data[3];
+        console.log(response.data[4])
       })
-      .catch(error => console.log(error));
-    }
+        .catch(error => console.log(error));
+      },
+      submitir(e){
+        e.preventDefault();
+        let currentObj = this;
+        const payload = {
+                    id_crime: this.crime,
+                    id_ent: this.ent,
+                    id_mun1: this.mun 
+                }
+        console.log(payload)
+        const url = 'https://spotlight-unfpa.datacivica.org/api/v1/timeline';
+        axios.post(url,payload)
+        .then(response => {currentObj.output = response.data;
+      })
+        .catch(error => console.log(error));
+      }
+    },
+          created() {
+            const payload = {id_crime:1,id_ent:14,id_mun1:39};
+            const url = 'https://spotlight-unfpa.datacivica.org/api/v1/timeline';
+            axios.post(url,payload)
+            .then(response => {this.posts = response.data[2];
+            this.info = response.data[2].total_ent;
+            this.info_ = response.data[5].year;
+            })
+            .catch(error => console.log(error));
+          }
 }
 
 </script>
